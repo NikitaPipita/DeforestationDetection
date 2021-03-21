57fa293cfca3ab61437d66daceed85f88b1656ad
Nikita Shuliak <artur.rejet.vseh@gmail.com>
2021-03-22 00:23:38 +0200

    User endpoints moved to user_endpoints file.

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,13 +1,11 @@
 package apiserver
 
 import (
-	"deforestation.detection.com/server/internal/app/model"
 	"deforestation.detection.com/server/internal/app/store"
 	"encoding/json"
 	"errors"
 	"github.com/gorilla/mux"
 	"net/http"
-	"strconv"
 )
 
 var (
@@ -43,163 +41,6 @@ func (s *server) configureRouter() {
 	s.router.HandleFunc("/user/{id:[0-9]+}/info", s.getUserByIdWithPassword()).Methods(http.MethodGet)
 	s.router.HandleFunc("/user/{id:[0-9]+}", s.updateUserById()).Methods(http.MethodPut)
 	s.router.HandleFunc("/user/{id:[0-9]+}", s.deleteUserById()).Methods(http.MethodDelete)
-}
-
-func (s *server) handleSessionsCreate() http.HandlerFunc {
-	type request struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		req := &request{}
-		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-
-		user, err := s.store.User().FindByEmail(req.Email)
-		if err != nil || !user.ComparePassword(req.Password) {
-			s.error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
-			return
-		}
-
-		s.respond(w, r, http.StatusOK, nil)
-	}
-}
-
-func (s *server) handleUsersCreate() http.HandlerFunc {
-	type request struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-		Role     string `json:"user_role"`
-		FullName string `json:"full_name"`
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		req := &request{}
-		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-
-		user := &model.User{
-			Email:    req.Email,
-			Password: req.Password,
-			Role:     req.Role,
-			FullName: req.FullName,
-		}
-		if err := s.store.User().Create(user); err != nil {
-			s.error(w, r, http.StatusUnprocessableEntity, err)
-			return
-		}
-
-		user.Sanitize()
-		s.respond(w, r, http.StatusCreated, user)
-	}
-}
-
-func (s *server) getAllUsers() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		users, err := s.store.User().GetAll()
-		if err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-
-		s.respond(w, r, http.StatusOK, users)
-	}
-}
-
-func (s *server) getUserById() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			s.error(w, r, http.StatusBadRequest, errIncorrectID)
-			return
-		}
-
-		user, err := s.store.User().FindByID(id)
-		if err != nil {
-			s.error(w, r, http.StatusNotFound, err)
-			return
-		}
-
-		s.respond(w, r, http.StatusOK, user)
-	}
-}
-
-func (s *server) getUserByIdWithPassword() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			s.error(w, r, http.StatusBadRequest, errIncorrectID)
-			return
-		}
-
-		user, err := s.store.User().FindByIDWithPassword(id)
-		if err != nil {
-			s.error(w, r, http.StatusNotFound, err)
-			return
-		}
-
-		s.respond(w, r, http.StatusOK, user)
-	}
-}
-
-func (s *server) updateUserById() http.HandlerFunc {
-	type request struct {
-		Email    string `json:"email"`
-		Role     string `json:"user_role"`
-		FullName string `json:"full_name"`
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			s.error(w, r, http.StatusBadRequest, errIncorrectID)
-			return
-		}
-
-		req := &request{}
-		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, r, http.StatusBadRequest, err)
-			return
-		}
-
-		user := &model.User{
-			Email:    req.Email,
-			Role:     req.Role,
-			FullName: req.FullName,
-		}
-		if err := s.store.User().Update(id, user); err != nil {
-			s.error(w, r, http.StatusUnprocessableEntity, err)
-			return
-		}
-
-		s.respond(w, r, http.StatusOK, user)
-	}
-}
-
-func (s *server) deleteUserById() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id, err := strconv.Atoi(vars["id"])
-		if err != nil {
-			s.error(w, r, http.StatusBadRequest, errIncorrectID)
-			return
-		}
-
-		if err := s.store.User().Delete(id); err != nil {
-			s.error(w, r, http.StatusNotFound, err)
-			return
-		}
-
-		s.respond(w, r, http.StatusNoContent, nil)
-	}
 }
 
 func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
