e418fcd5fc82e6eb09d7eb7f110f4dddc3905e0f
Nikita Shuliak <artur.rejet.vseh@gmail.com>
2021-03-22 20:29:45 +0200

    Added user role validation.

--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -16,22 +16,38 @@ type User struct {
 }
 
 func (u *User) Validate() error {
-	return validation.ValidateStruct(
+	if err := validation.ValidateStruct(
 		u,
 		validation.Field(&u.Email, validation.Required, is.Email),
 		validation.Field(&u.Password, validation.By(requiredIf(u.EncryptedPassword == "")), validation.Length(6, 100)),
 		validation.Field(&u.Role, validation.Required),
 		validation.Field(&u.FullName, validation.Required),
-	)
+	); err != nil {
+		return err
+	}
+
+	return u.ValidateRole()
 }
 
 func (u *User) UpdateValidate() error {
-	return validation.ValidateStruct(
+	if err := validation.ValidateStruct(
 		u,
 		validation.Field(&u.Email, validation.Required, is.Email),
 		validation.Field(&u.Role, validation.Required),
 		validation.Field(&u.FullName, validation.Required),
-	)
+	); err != nil {
+		return err
+	}
+
+	return u.ValidateRole()
+}
+
+func (u *User) ValidateRole() error {
+	if u.Role == "admin" || u.Role == "manager" || u.Role == "employee" || u.Role == "observer" || u.Role == "locked" {
+		return nil
+	}
+
+	return ErrIncorrectRole
 }
 
 func (u *User) BeforeCreate() error {
